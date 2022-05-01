3b0fac5cc4b9d14862b3b9c984c2e25511320036
blancc-page <blancc.666@gmail.com>
2022-05-01 23:56:14 +0300

    feat[model]: add news source model

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, request, redirect, url_for
 from app import create_app
-from ..request import get_sources
-# from ..models import review
+from ..request import get_sources, get_articles
+# from ..models import Article
 # from .forms import ReviewForm
 from . import main
+
+# Article = article.Article
 
 @main.route("/")
 def index():
@@ -13,7 +15,19 @@ def index():
     title = 'Home - Welcome to News App'
     
     source = get_sources()
-    
-    print("ww")
+    # print(source)
     
     return render_template("index.html", title = title, source = source)
+
+@main.route("/source/<id>")
+def article(id):
+    """
+    View article page function that returns the article details page
+    """
+    
+    article = get_articles(id)
+    title = f"{article[0].title}"
+    print(article[0].source)
+    
+    
+    return render_template("article.html", article = article, title = title)
--- app/models/article.py
+++ app/models/article.py
@@ -1,17 +1,3 @@
-class Source:
-    '''
-    Source class to define Source Objects
-    '''
-
-    def __init__(self,id,name,description,language):
-        self.id =id
-        self.name = name
-        self.desc = description
-        self.lang = language
-
-
-
-
 class Article:
     '''
     Article class to define Article Objects
