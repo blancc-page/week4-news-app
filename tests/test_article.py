cfc7c3c07a825cd925ff2e32b0d8308dfd437333
Moses Muta <65918325+blancc-page@users.noreply.github.com>
2022-05-09 11:27:28 +0300

    refactor[article_test]: improve test code

--- tests/test_article.py
+++ tests/test_article.py
@@ -16,13 +16,37 @@ class TestArticle(unittest.TestCase):
         Set up method to run before each test case
         
         """
-        self.new_article = Article("abc-news","ABC News","Lorem10","en")
+        self.new_article = Article(
+        "https://ichef.bbci.co.uk/news/1024/branded_news/C7A9/production/_124631115_dmytro.png",
+        "Ukraine war: 'If this is true, then I am also a Nazi'",
+        "Holocaust survivors in the Ukrainian city of Uman feel insulted by Russian claims to be fighting Nazis.",
+        "http://www.bbc.co.uk/news/world-europe-61361827",
+        "2022-05-09T07:52:17.5346426Z",
+        "BBC News")
         
     def test_init(self):
         """
         test_init test case to test if the object is initialized properly
         """
-        self.assertEqual(self.new_article.id, "abc-news")
-        self.assertEqual(self.new_article.name, "ABC News")
-        self.assertEqual(self.new_article.desc, "Lorem10")
-        self.assertEqual(self.new_article.lang, "en")
+        self.assertEqual(self.new_article.image, "https://ichef.bbci.co.uk/news/1024/branded_news/C7A9/production/_124631115_dmytro.png")
+        self.assertEqual(self.new_article.title, "Ukraine war: 'If this is true, then I am also a Nazi'")
+        self.assertEqual(self.new_article.desc, "Holocaust survivors in the Ukrainian city of Uman feel insulted by Russian claims to be fighting Nazis.")
+        self.assertEqual(self.new_article.url, "http://www.bbc.co.uk/news/world-europe-61361827")
+        self.assertEqual(self.new_article.publishedAt, "2022-05-09T07:52:17.5346426Z")
+        self.assertEqual(self.new_article.source, "BBC News")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        CAN YOU HEAR ME???
